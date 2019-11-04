f6a3a576f9f6f48397f1b5edd048ad2b58a12c20
matteo0026 <marmotta200026@gmail.com>
2019-11-04 21:19:38 +0100

    Now sources are good. Compile kernel and add useful stuff.

--- omni_milletlte.mk
+++ omni_milletlte.mk
@@ -1,4 +1,16 @@
+# Release name
+PRODUCT_RELEASE_NAME := milletlte
+
+# Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Time Zone data for Recovery
+PRODUCT_COPY_FILES += \
+    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+
 PRODUCT_DEVICE := milletlte
 PRODUCT_NAME := omni_milletlte
 PRODUCT_BRAND := samsung
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo omni_milletlte-eng
+add_lunch_combo omni_milletlte-userdebug
